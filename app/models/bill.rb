81d94441e7f11e1a1c2b21aae0fafc7d0b9b71d2
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-12-15 10:00:05 -0500

    create Bills table

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,6 @@
 class Bill < ApplicationRecord
+
+	belongs_to :tenant
+
+
 end
--- db/migrate/20171215144652_create_bills.rb
+++ db/migrate/20171215144652_create_bills.rb
@@ -1,6 +1,13 @@
 class CreateBills < ActiveRecord::Migration[5.1]
   def change
     create_table :bills do |t|
+    	t.float :amount_due
+    	t.string :due_date
+    	t.boolean :overdue?, default: false
+    	t.boolean :paid?, default: false
+    	t.string :payment_method
+
+    	t.belongs_to :tenant
 
       t.timestamps
     end
