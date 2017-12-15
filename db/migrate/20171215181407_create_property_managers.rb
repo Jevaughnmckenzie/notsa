0ff52b4a6e3ea95a8743db62da16ab332b6978be
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-12-15 13:45:50 -0500

    fix syntax errors in CreatePropertyManagers

--- db/migrate/20171215181407_create_property_managers.rb
+++ db/migrate/20171215181407_create_property_managers.rb
@@ -1,10 +1,10 @@
 class CreatePropertyManagers < ActiveRecord::Migration[5.1]
   def change
     create_table :property_managers do |t|
-      t.string, :name
-      t.string, :email
-      t.string, :password_digest
-      t.string, :phone_number
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :phone_number
       t.string :company
 
       t.timestamps
