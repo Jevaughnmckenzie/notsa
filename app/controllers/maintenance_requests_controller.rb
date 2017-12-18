b8fc4a5915c6058316c3d53eb59cf8e08527b97e
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-12-18 17:37:49 -0500

    create logic for 'create' action for  maintenance requests

--- app/controllers/maintenance_requests_controller.rb
+++ app/controllers/maintenance_requests_controller.rb
@@ -2,4 +2,20 @@ class MaintenanceRequestsController < ApplicationController
 	def new
 		@request = MaintenanceRequest.new
 	end
+
+	def create
+		@request = current_user.maintenance_requests.build(subject: maintenance_requests_params[:subject], description: maintenance_requests_params[:description], completed?: false, property: current_user.property)
+
+		if @request.save
+			redirect_to @request
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def maintenance_requests_params
+		params.require(:maintenance_request).permit(:subject, :description)
+	end
 end
