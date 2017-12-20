22a51774eb3c750a7d90b08ee3bd43d3ed8037d6
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-12-19 23:20:23 -0500

    add ability for property managers to create tenants for their properties

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,12 +1,27 @@
 class TenantsController < ApplicationController
 	
+	def new
+		@tenant = Tenant.new
+	end
+
+	def create
+		@tenant = Tenant.new(tenant_params)
+		@tenant.property = current_user.properties[rand(current_user.properties.count)]
+		if @tenant.save
+			redirect_to @tenant
+		else		
+			render 'new'
+		end
+	end
 
 	def show
-		if session[:email]
+		if current_user.is_a?(Tenant)
 
-			@user = current_user
+			@tenant = current_user
 			@property = current_user.property
-			
+		elsif current_user.is_a?(PropertyManager)
+			@tenant = Tenant.find(params[:id])
+			@property = @tenant.property
 		else
 			render '/login'
 		end
