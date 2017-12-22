09defdc3f8750997b3d5eb68e20fb604e5e26960
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-12-22 00:51:54 -0500

    refactor routes for tenant registration and sessions using namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,33 @@ Rails.application.routes.draw do
 
  
 
-  get '/signup/tenants', to: 'tenants/registrations#new'
-  post '/signup/tenants', to: 'tenants/registrations#create'
-  
-  get '/login/tenants', to: 'tenants/sessions#new'
-  post '/login/tenants', to: 'tenants/sessions#create'
+  # get '/signup/tenants', to: 'tenants/registrations#new'
+  # post '/signup/tenants', to: 'tenants/registrations#create'
+
+  namespace :tenants do
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create]
+  end
+
+   resources :tenants, only: [:show, :new, :create]
+
+  # get '/login/tenants', to: 'tenants/sessions#new'
+  # post '/login/tenants', to: 'tenants/sessions#create'
   delete '/destroy/tenants', to: 'tenants/sessions#destroy'
 
   get '/signup/property_managers', to: 'property_managers/registrations#new'
   post '/signup/property_managers', to: 'property_managers/registrations#create'
+
+  # scope '/signup', module: 'property_managers' do
+  #   resources :registrations, only: [:new, :create]
+  # end
 
   get '/login/property_managers', to: 'property_managers/sessions#new'
   post '/login/property_managers', to: 'property_managers/sessions#create'
   delete '/property_manager/destroy', to: 'property_managers/sessions#destroy'
 
   resources :properties, only: [:index, :show, :new, :create]
-  resources :tenants, only: [:show, :new, :create]
+ 
   resources :property_managers, only: [:show]
   resources :maintenance_requests, only: [:index, :new, :create, :show, :update, :destroy]
   resources :invoices, only: [:index, :show, :update]
