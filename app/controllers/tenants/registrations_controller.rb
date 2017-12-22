85664f551cb731497aabbe9bfa7b43e2fbf85c6a
jevaughnmckenzie <jmckenzie1004@gmail.com>
2017-12-22 02:55:37 -0500

    create logic that automatically creates a new invoice for a newly registered tenant

--- app/controllers/tenants/registrations_controller.rb
+++ app/controllers/tenants/registrations_controller.rb
@@ -10,6 +10,8 @@ class Tenants::RegistrationsController < ApplicationController
 		@tenant = Tenant.new(tenant_params)
 		@tenant.property = Property.all[rand(Property.count)]
 		create_user_model(@tenant)
+		@tenant.invoices.create(amount_due: 1000,
+															due_date: DateTime.now >> 1)
 	end
 
 end
